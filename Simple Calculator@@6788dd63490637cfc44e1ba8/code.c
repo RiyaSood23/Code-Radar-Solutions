f3d1d7c41379d82e3decf8db9577d8e86269fb97
RiyaSood23 <riyasood623@gmail.com>
2025-02-08 13:50:38 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,33 +1,66 @@
+// #include <stdio.h>
+
+// char* welcome() {
+//     return "Welcome to Code Radar!";
+// }
+
+// int main() {
+//     int a,b;
+//     char x;
+//     scanf("%d %d %c",&a,&b,&x);
+//     switch (x){
+//     case '+' : //( x='+'){
+//         {
+//             printf("%d",a+b);
+//             break;
+//     }
+//     case '-': //( x='-'){
+//         {
+//         printf("%d",a-b);
+//         break;
+//     }
+//     case '*': //( x='*')
+//     {
+//         printf("%d",a*b);
+//         break;
+//     }
+//     case '/' : //( x="/")
+//     {
+//         printf("%d",a/b);
+//         break;
+//     }}
+//     return 0;
+// }
 #include <stdio.h>
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
 int main() {
-    int a,b;
-    char x;
-    scanf("%d %d %c",&a,&b,&x);
-    switch (x){
-    case '+' : //( x='+'){
-        {
-            printf("%d",a+b);
+    char op;
+    double num1, num2;
+    
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch(op) {
+        case '+':
+            printf("Result: %.2lf\n", num1 + num2);
             break;
+        case '-':
+            printf("Result: %.2lf\n", num1 - num2);
+            break;
+        case '*':
+            printf("Result: %.2lf\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("Result: %.2lf\n", num1 / num2);
+            else
+                printf("Error: Division by zero!\n");
+            break;
+        default:
+            printf("Invalid operator!\n");
     }
-    case '-': //( x='-'){
-        {
-        printf("%d",a-b);
-        break;
-    }
-    case '*': //( x='*')
-    {
-        printf("%d",a*b);
-        break;
-    }
-    case '/' : //( x="/")
-    {
-        printf("%d",a/b);
-        break;
-    }}
+
     return 0;
 }
